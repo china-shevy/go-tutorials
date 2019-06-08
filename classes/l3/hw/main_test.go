c29b1de98d37e9a17797a35db324c6690e8b8ddd
CreatCodeBuild <CreatCodeBuild@github.com>
2019-06-07 22:42:48 -0700

    hw revision

--- classes/l3/hw/main_test.go
+++ classes/l3/hw/main_test.go
@@ -19,16 +19,20 @@ func TestChallenge1(t *testing.T) {
 	}()
 
 	// The only requirement is that it accepts an io.Reader. You need to decide the storage mechanism.
-	err := server.Log(strings.NewReader(`{"time": "2019-01-06", "number": 123}`)); require.NoError(t, err)
+	// Question:
+	//		Should it accept io.ReadCloser instead?
+	err := server.Log(strings.NewReader(`{"time": "2019-01-06", "number": 123}`)); require.NoError(t, err)	// Notice this log is the same.
+	err := server.Log(strings.NewReader(`{"time": "2019-01-06", "number": 123}`)); require.NoError(t, err)	// Should you keep duplication?
 	err = server.Log(strings.NewReader(`{"time": "2019-01-05", "number": 124}`)); require.NoError(t, err)
 	err = server.Log(strings.NewReader(`{"time": "2019-01-04", "number": 125}`)); require.NoError(t, err)
 	err = server.Log(strings.NewReader(`{"time": "2019-01-03", "number": 126}`)); require.NoError(t, err)
 	err = server.Log(strings.NewReader(`{"time": "2019-01-02", "number": 127}`)); require.NoError(t, err)
 	err = server.Log(strings.NewReader(`{"time": "2019-01-01", "number": 128}`)); require.NoError(t, err)
 
-	// You should come out a search API.
+	// You should define a search API. It could have any signature.
 	// Find the record between 01-01 and 01-03 and return in logging order.
-	// Should it return errors? If so, what kind?
+	// Question:
+	//		Should it return errors? If so, what kind?
 	logs := server.Search(?)
 	require.Equal(t, logs, []string{
 		`{"time":"2019-01-03","number":126}`,  // Notice that JSON format is compact
@@ -43,4 +47,8 @@ func TestChallenge1(t *testing.T) {
 		`{"number":127}`,
 		`{"number":128}`
 	})
+
+	// Question:
+	//		Because this is a log system, should you trust the 'time' which users input? Should the system have its own time?
+	//		因为这是一个日志系统，你应该相信用户输入的时间吗？系统需要自己记录时间吗？ 
 }
